840fb51a62159a84afca5dc301509beeb6f544f6
lz0928 <1137556826@qq.com>
2019-09-28 19:35:57 +0800

    first commit

---
+++ app/src/main/java/com/cailuzheng/zhihuibeijing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cailuzheng.zhihuibeijing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
